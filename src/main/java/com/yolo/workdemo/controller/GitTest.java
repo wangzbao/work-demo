510391d12ffba68f297962ec4bb984b78f6f9d5c
wangzhan <wangzhanazn@163.com>
2021-08-01 19:52:53 +0800

    gitTest commit partial

--- src/main/java/com/yolo/workdemo/controller/GitTest.java
+++ src/main/java/com/yolo/workdemo/controller/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("-------");
         System.out.println("new dev_git_learning_yolo_20210801 branch");
+        System.out.println("partial commit");
     }
 }
