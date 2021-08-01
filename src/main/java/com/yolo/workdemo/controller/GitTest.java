60b561932cf7c654d1bf259bd4a77d2dc7a1e58b
wangzhan <wangzhanazn@163.com>
2021-08-01 17:34:25 +0800

    new branch

--- src/main/java/com/yolo/workdemo/controller/GitTest.java
+++ src/main/java/com/yolo/workdemo/controller/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
 
     public static void main(String[] args) {
         System.out.println("-------");
+        System.out.println("new dev_git_learning_yolo_20210801 branch");
     }
 }
