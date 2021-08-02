b1f8b4ed2cca49dee5298d2719b2518bfa4a6a85
wangzhan <wangzhanazn@163.com>
2021-08-02 21:23:35 +0800

    多个commit合一个第一次

--- src/main/java/com/yolo/workdemo/controller/GitTest.java
+++ src/main/java/com/yolo/workdemo/controller/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("-------");
         System.out.println("new dev_git_learning_yolo_20210801 branch");
         System.out.println("partial commit");
+        System.out.println("git stash test");
     }
 }
