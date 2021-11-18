846de0060ceb81ed98202e3304472956046d5a1e
ext.wangzhan1 <ext.wangzhan1@jd.com>
2021-11-18 15:27:36 +0800

    Revert ""分支2推送""
    This reverts commit bb791d23

--- src/main/java/com/yolo/workdemo/controller/GitTest.java
+++ src/main/java/com/yolo/workdemo/controller/GitTest.java
@@ -27,7 +27,6 @@ public class GitTest {
     public static void main(String[] args) throws Exception {
 
         System.out.println("分支一尝试第2次提交");
-        System.out.println("分支二提交1");
         String id = "201139431";
         Long realId = IdConvertUtil.getRealId(id, null);
         System.out.println(realId);
